009b7619b6e6fce64ebee3652bd2815fd94b304f
Pingvidas <44850647+Pingvidas@users.noreply.github.com>
2024-10-23 19:30:21 +0000

    Pridėtas tarpas tarp Foo ir Bar

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Vartotojas įveda skaičių
 # Vietoj skaičių, dalių iš 3, spausdinama "Foo".
 # Vietoj skaičių, dalių iš 5, spausdinama "Bar".
-# Vietoj skaičių, dalių iš 3 ir 5, spausdinama "FooBar".
+# Vietoj skaičių, dalių iš 3 ir 5, spausdinama "Foo Bar".
 # Kitu atveju, skaičius spausdinamas nepakitęs.
 
 n = int(input("Įveskite skaičių: "))
 if n % 3 == 0 and n % 5 == 0:
-    print("FooBar")
+    print("Foo Bar")
 elif n % 3 == 0:
     print("Foo")
 elif n % 5 == 0:
