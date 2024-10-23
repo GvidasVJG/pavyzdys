f6e28f4027e534bb1f7acc07f33261444cb5087f
GvidasVJG <62912063+GvidasVJG@users.noreply.github.com>
2024-10-23 15:43:03 +0000

    Aprašyta paprasta FooBar programos versija

---
+++ main.py
@@ -0,0 +1,17 @@
+# Foo Bar simple Programa
+
+# Vartotojas įveda skaičių
+# Vietoj skaičių, dalių iš 3, spausdinama "Foo".
+# Vietoj skaičių, dalių iš 5, spausdinama "Bar".
+# Vietoj skaičių, dalių iš 3 ir 5, spausdinama "FooBar".
+# Kitu atveju, skaičius spausdinamas nepakitęs.
+
+n = int(input("Įveskite skaičių: "))
+if n % 3 == 0 and n % 5 == 0:
+    print("FooBar")
+elif n % 3 == 0:
+    print("Foo")
+elif n % 5 == 0:
+    print("Bar")
+else:
+    print(n)
